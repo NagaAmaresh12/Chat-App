5bc0762dfae9204192cdc2c0d7254f6682dcc6a4
KANNE NAGA AMARESH <kannenagaamaresh12@gmail.com>
2025-11-12 19:18:01 +0530

    added frontend folder and react+shadcn+typescript setup is done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button.tsx";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="" variant={"secondary"}>
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
